0aae755f1cac9237236a46efb45819f7f7b92aed
westonkd <wdrans@gmail.com>
2021-11-24 10:40:19 -0700

    Working col expansions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import tinymce from "tinymce";
+import { useEffect, useState } from "react";
+
+import Cell from "./Cell";
 
 function App() {
+  const [rows, setRows] = useState(1);
+  const [columns, setColumns] = useState(1);
+  const [cellCount, setCellCount] = useState(1);
+
+  useEffect(() => {
+    tinymce.init({
+      inline: true,
+      selector: ".cell",
+      plugins: "noneditable",
+    });
+  }, [cellCount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          borderBottom: "solid 2px gray",
+          padding: "10px",
+          marginBottom: "10px",
+        }}
+      >
+        <label>
+          Rows
+          <input
+            type="number"
+            min="1"
+            value={rows}
+            onChange={(e) => setRows(e.currentTarget.value)}
+          />
+        </label>
+        <div style={{ marginLeft: "10px", display: "inline" }}>
+          <label>
+            Columns
+            <input
+              type="number"
+              min="1"
+              value={columns}
+              onChange={(e) => setColumns(e.currentTarget.value)}
+            />
+          </label>
+        </div>
+      </div>
+
+      <div
+        className="App"
+        style={{
+          gridTemplateRows: `repeat(${rows}, auto)`,
+          gridTemplateColumns: `repeat(${columns}, auto)`,
+        }}
+      >
+        {Array.from(Array(cellCount).keys()).map((index) => {
+          return (
+            <Cell
+              rows={rows}
+              columns={columns}
+              index={index}
+              key={`CEL-${index}`}
+            />
+          );
+        })}
+        <div className="addCell" onClick={() => setCellCount((old) => old + 1)}>
+          + Add Cell
+        </div>
+      </div>
+      <div>End Content</div>
+    </>
   );
 }
 
